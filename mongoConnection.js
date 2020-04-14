5f090a9d8b79af34d08ac04d345bfb14641935d7
mytiesto <46444631+mytiesto@users.noreply.github.com>
2020-04-14 10:03:27 +0100

    Implementacao MongoDB

--- mongoConnection.js
+++ mongoConnection.js
@@ -12,11 +12,14 @@ var Utilizador = new Schema({
     email: String,
     password: String,
     nome: String,
-    genero: {type: String, maxlength: 1},
+    genero: {
+        type: String,
+        enum: ["Masculino", "Feminino", "Outro"]
+    },
     dataDeNascimento: Date,
     comissaoGestao: Boolean,
     aprovado: Boolean,
-    entidades: [entidadeId],
+    entidades: [{entidadeId: Entidade.entidadeId}],
     externo: Boolean,
     dataCriacao: Date,
     fotoPerfilCaminho: String,
@@ -30,4 +33,45 @@ var Inscricao = new Schema({
     presente: Boolean,
     avaliacao : [{valor: Number, descricao: String}],
     cancelado : Boolean,
+})
+
+var CategoriaProjeto = new Schema({
+    categoriaId: mongoose.ObjectId,
+    predefinido: Boolean,
+    nome: String
+})
+
+var PublicoAlvo = new Schema({
+    publicoAlvoId: mongoose.ObjectId,
+    descricao: String,
+    predefinido: Boolean
+})
+
+var Projeto = new Schema({
+    projetoId: mongoose.ObjectId,
+    nome: String,
+    resumo: String,
+    responsavelId: Utilizador.utilizadorId,
+    categorias: [{categoriaId: Categoria.categoriaId}],
+    palavrasChave: [{nome: String}],
+    contactos: [{contacto: String, descricao: String}],
+    publicoAlvoId: PublicoAlvo.publicoAlvoId,
+    formacoesNecessarias: [{nome: String}],
+    logotipoCaminho: String,
+    XemXTempo: String,
+    edicoes: [{
+        aprovado: Boolean,
+        gestores: [{gestorId: Utilizador.utilizadorId}],
+        comentarios: [{comentario: String, utilizadorId: Utilizador.utilizadorId, dataCriacao: Date}],
+        fotosCaminho: [{caminho: String}],
+        vagas: Number,
+        horarios: [{descricao: String, dataAcontecimento: Date}],
+        capaCaminho: String,
+        ficheirosCaminho: [{caminho: String}],
+        projetoMes: Boolean,
+        dataCriacao: Date,
+        dataTermino: Date,
+        dataFechoInscricoes: Date,
+        dataComeco: Date
+    }]
 })
